b59687adf8cee32f62543b649feea067768cfda3
Steve CHI-YUAN LENG <35572931+avgsteve@users.noreply.github.com>
2020-10-22 23:48:33 +0800

    adjust carousel layout

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -25,12 +25,12 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name} (${product.price})
               </h2>
             </Carousel.Caption>
+            <Image src={product.image} alt={product.name} fluid />
           </Link>
         </Carousel.Item>
       ))}
